8d710659d7560fd3b5978d4b80118a2bf47b8c33
Mahesh Ravi <maheshm444@gmail.com>
2021-11-18 18:12:05 +0530

    ADD:image slider

---
+++ src/components/Viewers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Viewers from './Viewers';
+
+function Viewers() {
+    return (
+        <div>
+         Viewers   
+        </div>
+    )
+}
+
+export default Viewers
